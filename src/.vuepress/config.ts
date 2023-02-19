967a74246368ca32ed32accec5e4aaeeec3b9f3b
yuzhihang <misaka10032@aliyun.com>
2023-02-19 15:07:51 +0800

    搜索功能

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,12 +1,26 @@
 import { defineUserConfig } from "vuepress";
+import { searchProPlugin, Page } from "vuepress-plugin-search-pro";
 import theme from "./theme";
 
 export default defineUserConfig({
+  lang: "zh-CN",
   base: "/frontend-service-station/",
-
   theme,
-
   title: "前端加油站",
-
   shouldPrefetch: false,
+  plugins: [
+    searchProPlugin({
+      indexContent: true,
+      customFields: [
+        {
+          getter: (page: Page) => page.frontmatter.category,
+          formatter: "分类：$content",
+        },
+        {
+          getter: (page: Page) => page.frontmatter.tag,
+          formatter: "标签：$content",
+        },
+      ],
+    }),
+  ],
 });
